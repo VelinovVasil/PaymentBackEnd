979ca6b9068b4d37062e4e46d0e7db00eec46c34
Vasil Velinov <velinov.vasil.i@gmail.com>
2023-08-29 20:24:27 +0300

    Initial commit

---
+++ src/main/java/tech/bonda/PaymentBackEnd/PaymentBackEndApplication.java
@@ -0,0 +1,13 @@
+package tech.bonda.PaymentBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/bonda/PaymentBackEnd/PaymentBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.bonda.PaymentBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
