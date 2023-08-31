d8b2142485d44892e05c1e283fdb05d200ba1c8d
bonda29 <bondableglobe10@gmail.com>
2023-08-31 18:57:12 +0300

    Added @Repository annotation.

--- src/main/java/tech/bonda/PaymentBackEnd/repository/CardRepository.java
+++ src/main/java/tech/bonda/PaymentBackEnd/repository/CardRepository.java
@@ -1,7 +1,9 @@
 package tech.bonda.PaymentBackEnd.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tech.bonda.PaymentBackEnd.entities.account.Card;
 
+@Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
 }
--- src/main/java/tech/bonda/PaymentBackEnd/repository/TransactionRepository.java
+++ src/main/java/tech/bonda/PaymentBackEnd/repository/TransactionRepository.java
@@ -1,7 +1,9 @@
 package tech.bonda.PaymentBackEnd.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tech.bonda.PaymentBackEnd.entities.account.Transaction;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
 }
